137a7adb523e7c038edc020967f3caee440bf6bc
Waleed Ghallab <waleedessam30@gmail.com>
2022-03-01 18:51:55 +0200

    Day1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+import { DiscountOffers } from './Shared Classes and types/discount-offers';
 import { Component } from '@angular/core';
-
+import { iProducts } from './Shared Classes and types/iProducts';
+import { ICategory } from './Shared Classes and types/i-category';
 @Component({
   selector: 'myfirstapp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+  
 })
+
 export class AppComponent {
   title = 'Angular_Day1';
+  
 }
